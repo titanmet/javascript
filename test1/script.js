b1d8630061fed0801ae5637d4d605a878670dff2
titanmet <titanmet@mail.ru>
2019-03-14 21:51:10 +0300

    test 1

--- test1/script.js
+++ test1/script.js
@@ -1,10 +1,6 @@
-var money = prompt("Ваш бюджет на месяц?",''),
+"use strict";
+var money = +prompt("Ваш бюджет на месяц?",''),
     time = prompt("Введите дату в формате YYYY-MM-DD",'');
-
-let a1=prompt("Введите обязательную статью расходов в этом месяце",''),
-    a2=prompt("Во сколько обойдется?",''),
-    a3=prompt("Введите обязательную статью расходов в этом месяце",''),
-    a4=prompt("Во сколько обойдется?",'');
 
 var appData = {
     budget: money,
@@ -15,8 +11,23 @@ var appData = {
     savings: false
 };
 
-appData.expenses.a1=a2;
-appData.expenses.a3=a4;
-
-alert(appData.budget/30);
-
+for(let i=0;i<2;i++){
+    let a=prompt("Введите обязательную статью расходов в этом месяце",''),
+        b=prompt("Во сколько обойдется?",'');
+    if((typeof(a))==='string' && (typeof(a))!=null && (typeof(b))!=null && a!='' && b!='' && a.length<50){
+        appData.expenses[a]=b;
+    }else{
+        alert("Произошла ошибка !");
+    }
+};
+appData.moneyPerDay=appData.budget/30;
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+if(appData.moneyPerDay<100){
+    console.log("Уровень дохода минимальный.");
+}else if(appData.moneyPerDay>100 && appData.moneyPerDay<2000){
+    console.log("Средний уровень достатка.");
+}else if(appData.moneyPerDay>2000){
+    console.log("Высокий уровень достатка.");
+}else{
+    console.log("Произошла ошибка!");
+};
