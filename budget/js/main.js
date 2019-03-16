36e3f63acdfdc7095cff793ac6e3977706ce0ac1
titanmet <titanmet@mail.ru>
2019-03-16 11:03:12 +0300

    budget

---
+++ budget/js/main.js
@@ -0,0 +1,34 @@
+let start = document.getElementById('start'),
+    budget = document.getElementsByClassName('.budget-value'),
+    budgetDay = document.getElementsByClassName('.daybudget-value'),
+    level = document.getElementsByClassName('.level-value'),
+    expenses = document.getElementsByClassName('.expenses-value'),
+    expensesExp = document.getElementsByClassName('.optionalexpenses-value'),
+    income = document.getElementsByClassName('.income-value'),
+    monthsaving = document.getElementsByClassName('.monthsavings-value'),
+    yearsaving = document.getElementsByClassName('.yearsavings-value'),
+    expensesItem = document.getElementsByClassName('.xpenses-item');
+    optionalexpenses = document.querySelectorAll('.optionalexpenses-item'),
+    buttonUtv = document.getElementsByTagName('button')[0],
+    buttonRs = document.getElementsByTagName('button')[1],
+    summa = document.querySelector('.choose-sum'),
+    percent = document.querySelector('.choose-percent'),
+    year = document.querySelector('.year-value'),
+    month = document.querySelector('.month-value'),
+    day = document.querySelector('.day-value'),
+    choiseIncome = document.querySelector('.choose-income'),
+    checkBox = document.querySelector('.checksavings');
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
