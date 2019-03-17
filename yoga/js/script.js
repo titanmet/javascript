b614df0084495b807b983c0b9cf78e60df18e2cb
titanmet <titanmet@mail.ru>
2019-03-17 11:37:43 +0300

    timer yoga

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -32,4 +32,44 @@ window.addEventListener('DOMContentLoaded', function() {
         }
     });
 
+// Timer
+
+let deadline = '2019-03-29';
+
+function getTimeRemaning(endtime){
+    let t=Date.parse(endtime) - Date.parse(new Date()),
+    seconds = Math.floor((t/1000) % 60),
+    minutes = Math.floor((t/1000/60) % 60),
+    hours = Math.floor(t/1000/60/60);  // t/(1000*60*60)
+//  day = Math.floor((t/1000/60/60)%24);
+
+return {
+    'total' : t,
+    'hours' : hours,
+    'minutes' : minutes,
+    'second' : seconds
+};
+
+}
+
+function setClock(id,endtime){
+    let timer = document.getElementById(id),
+    hours = timer.querySelector('.hours'),
+    minutes = timer.querySelector('.minutes'),
+    seconds = timer.querySelector('.seconds'),
+    timeInterval = setInterval(updateClock,1000);
+
+    function updateClock(){
+        let t = getTimeRemaning(endtime);
+        hours.textContent = t.hours;
+        minutes.textContent = t.minutes;
+        seconds.textContent = t.second;
+
+        if(t.total<=0){
+            clearInterval(timeInterval);
+        }
+    }
+}
+
+setClock('timer',deadline);
 });
