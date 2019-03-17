6d71118fdf746a7082cc51ed9ba6db851709afaf
titanmet <titanmet@mail.ru>
2019-03-17 21:26:52 +0300

    modal window

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -84,4 +84,22 @@ function setClock(id,endtime){
 }
 
 setClock('timer',deadline);
+
+// modal window
+
+let more = document.querySelector('.more'),
+    overlay = document.querySelector('.overlay'),
+    close = document.querySelector('.popup-close');
+
+more.addEventListener('click',function() {
+    overlay.style.display='block';
+    this.classList.add('more-splash'); 
+    document.body.style.overflow = 'hidden';  
+});
+close.addEventListener('click',function() {
+    overlay.style.display='none';
+    more.classList.remove('more-splash');
+    document.body.style.overflow = '';
+});
+
 });
