ac59d4f2bdc3e4099bce5cc05982975156be1a63
titanmet <titanmet@mail.ru>
2019-03-18 11:28:22 +0300

    calc

---
+++ calc/js/script.js
@@ -0,0 +1,22 @@
+let inputRub = document.getElementById("rub"),
+    inputUsd = document.getElementById("usd");
+
+inputRub.addEventListener('input', ()=> {
+    let request = new XMLHttpRequest();
+    // request.open(method,url,async,login.pass);
+    request.open('GET','js/current.json');
+    request.setRequestHeader('Content-type','application-json; charset=utf-8');
+    request.send();
+
+    request.addEventListener('readystatechange', function() {
+        if (request.readyState === 4 && request.status == 200){
+            let data = JSON.parse(request.response);
+            inputUsd.value = (inputRub.value / data.usd).toFixed(2);
+        } else {
+            inputUsd.value = 'Ошибка !';
+        }
+
+        
+    });
+
+});
