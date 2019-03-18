64d3187732e638f7570740d06f92c29f2d335450
titanmet <titanmet@mail.ru>
2019-03-18 20:20:50 +0300

    ajax form

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -119,7 +119,37 @@ let form = document.querySelector('.main-form'),
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     form.appendChild(statusMessage);
+
+    let request = new XMLHttpRequest();
+    request.open('POST','server.php');
+    // request.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+    request.setRequestHeader('Content-type','application/json; charset=utf-8');
     
+    let formData = new FormData(form);
+    
+
+    let obj = {};
+    formData.forEach(function(value,key) {
+        obj[key]=value;
+    });
+    let json = JSON.stringify(obj);
+
+    request.send(json);
+
+    request.addEventListener('readystatechange', function() {
+        if(request.readyState < 4) {
+            statusMessage.innerHTML = message.loading;
+        } else if(request.readyState === 4 && request.status == 200) {
+            statusMessage.innerHTML = message.success;
+        } else {
+            statusMessage.innerHTML = message.failure;
+        }
+    });
+
+    for (let i=0;i < input.length;i++){
+        input[i].value='';
+    }
+
 });
 
 });
