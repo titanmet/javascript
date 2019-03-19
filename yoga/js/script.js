c5a28fc9f9093dce4028db44ae44bbc7488bfbe6
titanmet <titanmet@mail.ru>
2019-03-19 21:56:43 +0300

    calck

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -206,4 +206,48 @@ form.addEventListener('submit', function(event) {
         }
     });
 
+// Calc
+
+let persons = document.querySelectorAll('.counter-block-input')[0],
+    restDays = document.querySelectorAll('.counter-block-input')[1],
+    place = document.getElementById('select'),
+    totalValue = document.getElementById('total'),
+    personsSum = 0,
+    daysSum = 0,
+    total = 0;
+
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener('change',function() {
+        personsSum = +this.value;
+        total = (daysSum + personsSum)*4000;
+
+        if(restDays.value == '') {
+            totalValue.innerHTML = 0; 
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    restDays.addEventListener('change',function() {
+        daysSum = +this.value;
+        total = (daysSum + personsSum)*4000;
+
+        if(persons.value == '') {
+            totalValue.innerHTML = 0; 
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change',function() {
+        if(persons.value == '' || restDays.value == ''){
+            totalValue.innerHTML = total;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    });
+
+
 });
