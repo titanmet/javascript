9fca36f51cd5b8046411f4ed07aef857ad937584
titanmet <titanmet@mail.ru>
2019-03-22 19:19:20 +0300

    webpack

--- yoga./webpack.config.js
+++ yoga./webpack.config.js
@@ -9,38 +9,39 @@ module.exports = {
   entry: './js/script.js',
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/js'
+    path: __dirname + 'js'
   },
   watch: true,
 
   devtool: "source-map",
 
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.js$/,
-  //       exclude: /(node_modules|bower_components)/,
-  //       use: {
-  //         loader: 'babel-loader?optional[]=runtime',
-  //         options: {
-  //           presets: [
-  //             ["@babel/env", {
-  //               targets: {
-  //                 edge: "17",
-  //                 firefox: "60",
-  //                 chrome: "67",
-  //                 safari: "11.1",
-  //                 ie: "11"
-  //               },
-  //               useBuiltIns: "usage"
-  //             }]
-  //           ]
-  //         }
-  //       }
-  //     }
-  //   ]
-  // },
-  // plugins: [
-  //   new UglifyJsPlugin()
-  // ]
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader?optional[]=runtime',
+          options: {
+            presets: [
+              ["@babel/env", {
+                targets: {
+                  edge: "17",
+                  firefox: "60",
+                  chrome: "67",
+                  safari: "11.1",
+                  ie: "11"
+                },
+                useBuiltIns: "usage"
+              }]
+            ]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new UglifyJsPlugin()
+  ]
 };
+
